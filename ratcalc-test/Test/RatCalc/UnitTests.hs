937be6c422192042a10314304cfd63ccb672798f
jim.farrand-sourcecode@xyxyx.org <jim@xyxyx.org>
2011-12-07 20:56:28 +0000

    Copyright check should use cached text
    - Use git grep in copyright check, so that we can check text that will
      actually be checked in
    - Add copyright to UnitTests

--- ratcalc-test/Test/RatCalc/UnitTests.hs
+++ ratcalc-test/Test/RatCalc/UnitTests.hs
@@ -1,3 +1,22 @@
+{-
+-- RatCalc - An infinite precision numeric computation framework
+-- Copyright (C) 2011 Jim Farrand
+--
+-- This program is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option)
+-- any later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+-- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+-- more details.
+--
+-- You should have received a copy of the GNU General Public License along with
+-- this program.  If not, see <http://www.gnu.org/licenses/
+-}
+
+{- Unit tests for everything -}
 
 {-# OPTIONS_GHC -XDoAndIfThenElse #-}
 
@@ -6,7 +25,10 @@ module Test.RatCalc.UnitTests where
 import Test.HUnit
 import System.Exit
 
-tests = TestList []
+tests =
+    TestList
+        [
+        ]
 
 runTests = runTestTT tests
 
