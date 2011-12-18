8a8011cbd88b9eaf7cd185dfea2234df2b060b23
jim.farrand-sourcecode@xyxyx.org <jim@xyxyx.org>
2011-12-18 23:06:21 +0000

    Minor cleanups

---
+++ ratcalc-test/RatCalc/Functions/Trigonometry/PrintPi.hs
@@ -0,0 +1,32 @@
+{-
+-- RatCalc - An infinite precision numeric computation framework
+-- Copyright (C) 2011 Jim Farrand
+--
+-- This program is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option)
+-- any later version.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+-- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+-- more details.
+--
+-- You should have received a copy of the GNU General Public License along with
+-- this program.  If not, see <http://www.gnu.org/licenses/
+-}
+
+
+import RatCalc.Estimator
+import RatCalc.Functions.Trigonometry as Trigonometry
+import System.IO
+
+main = printStrLnFlush (showEstimatorDigits 10 (-1) Trigonometry.pi)
+
+printStrLnFlush (c:cs) = do
+    putChar c
+    hFlush stdout
+    printStrLnFlush cs
+printStrLnFlush [] = do
+    putChar '\n'
+    hFlush stdout
