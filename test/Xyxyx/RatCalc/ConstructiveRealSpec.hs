ba11002efe1137ca91ba5d699318177713bbd0e4
Jim Farrand <jim.farrand@xyxyx.org>
2017-04-18 21:51:11 +0100

    Add test for addition

--- test/Xyxyx/RatCalc/ConstructiveRealSpec.hs
+++ test/Xyxyx/RatCalc/ConstructiveRealSpec.hs
@@ -8,4 +8,6 @@ spec = do
     describe "Constructive Reals" $ do
         it "rationals approximate to correct value" $ property $
             \r -> approximate (rational r) == fromRational r
+        it "added rationals approximate to correct value" $ property $
+            \x y -> approximate ((rational x) `add` (rational y)) == fromRational (x+y)
 
